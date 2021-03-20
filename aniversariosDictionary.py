a06e4b5394dda5336f3471964cffef884abbb3c6
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2021-03-20 20:26:05 -0300

    Aniversário do Falci add.

--- aniversariosDictionary.py
+++ aniversariosDictionary.py
@@ -1,7 +1,8 @@
 aniversarios = {
 "Felipe": "12 Julho",
 "Henrique": "25 Janeiro",
-"Lucas": "7 Setembro"}
+"Lucas": "7 Setembro",
+"Falcirolli": "10 de Abril"}
 
 while True:
     print("Digite um nome: (deixe em branco para sair)")
@@ -21,3 +22,26 @@ while True:
             print("Digite o dia do aniversário de " + name)
             dia = input()
             aniversarios[name]= dia
+
+print("Usando .values() ")
+#imprimi tds os values do dictionary (dados do lado direito)
+for i in aniversarios.values():
+    print (i)
+print("----------")
+print("Usando .keys()")
+#imprimi tds as keys do dictionary (dados do lado esquerdo)
+for i in aniversarios.keys():
+    print(i)
+print("----------")
+print("Usando .items()")
+#imprimi tds as combinações de dados do dictionary
+for i in aniversarios.items():
+    print (i)
+print("----------")
+print("Printando o dictionary")
+print(aniversarios)
+
+print("----------")
+
+for i,j in aniversarios.items():
+    print("Key: " + i + " | Value: " + j)
