9bbb9c9a4d8bd690e9dbaec4fdfeffa9323bf4d7
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2021-03-23 00:05:35 -0300

    Comentei o codigo para poder adicionar outro e testar.

---
+++ manipulacaoString.py
@@ -0,0 +1,25 @@
+print(
+'''alou
+td bem?''')
+oi = "Hello World!"
+print(oi[0])
+print(oi[5])
+print(oi[8])
+print(oi[0:5])
+print(oi[6:])
+
+# while True:
+#     idade = input("Digite sua idade: ")
+
+#     if idade.isnumeric():
+#         break
+#     print("Digite um número inteiro*******")
+
+# while True:
+#     senha = input("Digite uma nova senha (letras e numeros, apenas): ")
+
+#     if senha.isalnum():
+#         break
+
+#     print("Senhas só podem conter letras e números")
+# print(idade + "\n" + senha)
