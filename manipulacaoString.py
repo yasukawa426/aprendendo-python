4ed90cd05e090f20e43b86446882b05c1751e8fa
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2021-03-23 00:31:44 -0300

    funcao criada q formata a exibicao de um picnic.

--- manipulacaoString.py
+++ manipulacaoString.py
@@ -43,6 +43,21 @@ print("Hello".ljust(20, "*"))
 print("Hello".center(20, "=")) 
 
 
+def printPicnic(itemsDict, leftWidth, rightWidth): 
+    print('PICNIC ITEMS'.center(leftWidth + rightWidth, '-')) 
+    #para cada par de chave-valor, imprime eles formatados
+    for chave, valor in itemsDict.items(): 
+        print(chave.ljust(leftWidth, '.') + str(valor).rjust(rightWidth)) 
+        
+
+picnicItems = {
+'sandwiches': 4,
+'apples': 12, 
+'cups': 4, 
+'cookies': 8000} 
+printPicnic(picnicItems, 12, 5) 
+printPicnic(picnicItems, 20, 6)
+
 
 
 # while True:
