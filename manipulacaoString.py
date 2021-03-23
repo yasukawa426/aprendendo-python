82356a64646a690bf701c745743818637436ecd7
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2021-03-23 00:12:39 -0300

    Metodos join() e split() add.

--- manipulacaoString.py
+++ manipulacaoString.py
@@ -8,6 +8,18 @@ print(oi[8])
 print(oi[0:5])
 print(oi[6:])
 
+nomes = ['Carlos', 'Michael', "gAy", "noses"]
+
+print (' '.join(nomes))
+print (' e '.join(nomes))
+print(', '.join(nomes))
+
+frase = "Oi, eu sou eu maa"
+
+print(frase.split())
+print(frase.split("u"))
+print(frase.split(sep = "u"))#msm coisa
+
 # while True:
 #     idade = input("Digite sua idade: ")
 
@@ -23,3 +35,4 @@ print(oi[6:])
 
 #     print("Senhas só podem conter letras e números")
 # print(idade + "\n" + senha)
+
